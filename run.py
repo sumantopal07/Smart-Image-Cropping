f38889aa26ca614aa6ccb4491af493cf786d0595
sumanto pal <sumantopal07@gmail.com>
2020-11-22 19:11:17 +0530

    deploy again

--- run.py
+++ run.py
@@ -234,9 +234,9 @@ def upload_image():
     return render_template("/public/upload_image.html")
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0', port=port)
 
 
 
